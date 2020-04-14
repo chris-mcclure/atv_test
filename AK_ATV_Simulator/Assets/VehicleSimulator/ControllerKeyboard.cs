58e7546afcd8ce1bf72f2fdb6d82cc74f021ff45
Jacob <jacobrwguenther@protonmail.com>
2020-04-14 13:25:19 -0800

    Add vehicle reset on 'r' press to FixedUpdate.

--- AK_ATV_Simulator/Assets/VehicleSimulator/ControllerKeyboard.cs
+++ AK_ATV_Simulator/Assets/VehicleSimulator/ControllerKeyboard.cs
@@ -25,6 +25,14 @@ public class ControllerKeyboard : MonoBehaviour
             if (Input.GetKey ("a")) { rotate=-1.0f; }
             if (Input.GetKey ("d")) { rotate=+1.0f; }
             vehicle.complementary_filter(0.01f,ref vehicle.cur_steer,rotate);
+
+            // Reset (after flip)
+            if (Input.GetKey("r")) {
+                vehicle.transform.position=vehicle.flat_Y(vehicle.transform.position);
+                vehicle.transform.LookAt(vehicle.flat_Y(vehicle.transform.position+transform.forward*10.0f));
+                vehicle.get_rb().velocity=Vector3.ClampMagnitude(vehicle.get_rb().velocity,5.0f); // limit linear velocity (don't zero it, for ice)
+                vehicle.get_rb().angularVelocity=new Vector3(0.0f,0.0f,0.0f);
+            }
         }
     }
 }
