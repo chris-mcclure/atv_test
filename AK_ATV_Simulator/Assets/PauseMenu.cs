61f94a1b9c5cb909f2aede9f529fc9be651efe1f
Chris McClure <chrismcclure2010@gmail.com>
2020-04-18 19:46:45 -0800

    updated pausemenu

--- AK_ATV_Simulator/Assets/PauseMenu.cs
+++ AK_ATV_Simulator/Assets/PauseMenu.cs
@@ -7,15 +7,17 @@ public class PauseMenu : MonoBehaviour
 {
     // Start is called before the first frame update
     public static bool GameIsPaused = false;
+    public bool inOptionsMenu = false;
     public GameObject pauseMenuUI;
+    public GameObject optionsUI;
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.P)){
-            if(GameIsPaused){
+            if(GameIsPaused && !inOptionsMenu){
                 Resume();
             }
-            else{
+            else if(!inOptionsMenu){
                 Pause();
             }
         }
@@ -25,11 +27,25 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
-
+    
     void Pause(){
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
+    }
+
+    public void LoadOptions(){
+        inOptionsMenu = true;
+        GameIsPaused = true;
+        Time.timeScale = 0f;
+        pauseMenuUI.SetActive(false);
+        optionsUI.SetActive(true);
+    }
+
+    public void BackButton(){
+        optionsUI.SetActive(false);
+        pauseMenuUI.SetActive(true);
+        inOptionsMenu = false;
     }
 
     public void LoadMenu(){
